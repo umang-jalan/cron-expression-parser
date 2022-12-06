096283f459d4940fbb5ee2f0b2b49f74e11314f6
Umang Jalan <umang@mykaam.app>
2022-12-06 16:56:02 +0530

    add core logic in CronExpressionParser

--- src/main/java/com/cronexpressionparser/CronExpressionParser.java
+++ src/main/java/com/cronexpressionparser/CronExpressionParser.java
@@ -1,6 +1,10 @@
 package com.cronexpressionparser;
 
 import com.cronexpressionparser.exceptions.InvalidCronExpression;
+import com.cronexpressionparser.timefield.AbstractTimeField;
+import com.cronexpressionparser.timefield.TimeFieldFactory;
+
+import java.util.List;
 
 public class CronExpressionParser {
 
@@ -10,6 +14,44 @@ public class CronExpressionParser {
             throw new InvalidCronExpression("cron expression must have 6 parts");
         }
 
-        return null;
+        CronExpressionResult cronExpressionResult = new CronExpressionResult();
+        for (int position=0; position < 5; position++){
+            String timeSubExpression = subExpressions[position];
+            AbstractTimeField abstractTimeField = TimeFieldFactory.getTimeField(position, timeSubExpression);
+            List<Integer> values = abstractTimeField.process();
+            updateCronExpression(cronExpressionResult, abstractTimeField, values);
+        }
+
+        // command can have more than one parts e,g, /usr/bin/run arg1 arg2 is a single command
+        StringBuilder commandBuilder = new StringBuilder();
+        for (int position=5; position < subExpressions.length;position++){
+            commandBuilder.append(subExpressions[position]);
+            if (position != subExpressions.length - 1) {
+                commandBuilder.append(" ");
+            }
+        }
+
+        cronExpressionResult.setCommand(commandBuilder.toString());
+        return cronExpressionResult;
+    }
+
+    private void updateCronExpression(CronExpressionResult cronExpressionResult, AbstractTimeField abstractTimeField, List<Integer> values){
+        switch (abstractTimeField.getType()){
+            case MINUTE:
+                cronExpressionResult.setMinutes(values);
+                break;
+            case HOUR:
+                cronExpressionResult.setHours(values);
+                break;
+            case DAY_OF_MONTH:
+                cronExpressionResult.setDaysOfMonth(values);
+                break;
+            case MONTH:
+                cronExpressionResult.setMonths(values);
+                break;
+            case DAY_OF_WEEK:
+                cronExpressionResult.setDaysOfWeek(values);
+                break;
+        }
     }
 }
